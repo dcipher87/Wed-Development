167421abeed822085be3bd862c909003149a072b
Khaya Ubisi <dcipher87@gmail.com>
2022-04-12 17:46:54 +0200

    I don't even know, there's just a lot here man

--- Portfolio/freeCodeCamp/Portfolio - Kevin Powell/js/index.js
+++ Portfolio/freeCodeCamp/Portfolio - Kevin Powell/js/index.js
@@ -1,15 +1,16 @@
+// TEST TO SEE THAT JS FILE IN LINKED TO HTML
+// const navToggle = document.querySelector('.nav-toggle');
+//
+// navToggle.addEventListener('click', () => {
+//   document.body.classList.toggle('nav-open');
+// });
+
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('nav__link');
+const navLinks = document.querySelectorAll('.nav__link');
 
 navToggle.addEventListener('click', () => {
   document.body.classList.toggle('nav-open');
 });
-
-// navLinks.forEach(link => {
-//   link.addEventListener('click', () => {
-//     document.body.classList.remove('nav-open');
-//   })
-// })
 
 navLinks.forEach(link => {
   link.addEventListener('click', () => {
